4a9278e0592c87c326d76bc3403f9c8ee3b16e14
adrianrcc <adrian.ramirez.cruzado@gmail.com>
2021-05-07 16:30:26 +0200

    .

---
+++ install_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# Instalación de docker y docker-compose #
+
+# ----------------------------#
+#         Instalación         #
+# ----------------------------#
+
+# Habilitamos el modo de shell para mostrar los comandos que se ejecutan #
+set -x
+
+# Actualizamos repositorios #
+apt update
+
+# Instalamos docker #
+apt install docker -y
+
+# Instalamos docker-compose #
+docker-compose -y
+
+# Habilitamos docker y arrancamos servicio #
+systemctl enable docker
+systemctl start docker
+
+# Lanzamos docker-compose yml en modo detached #
+docker-compose up -d
